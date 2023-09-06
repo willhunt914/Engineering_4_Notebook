15d3179e61632d9aaaf680c2b50fc9918b83d571
willhunt914 <whunt29a@charlottesvilleschools.org>
2023-09-06 10:44:21 -0400

    kjhgfds

---
+++ raspberry-pi/launchpad1.py
@@ -0,0 +1,10 @@
+import time
+import board
+import digitalio
+from time import sleep
+
+for i in range (10,0,-1):
+    print(i)
+    sleep(1)
+print("liftoff")
+
---
+++ raspberry-pi/ledtest.py
@@ -0,0 +1,14 @@
+import board# type: ignore
+import digitalio# type: ignore
+from time import sleep
+led = digitalio.DigitalInOut(board.LED) 
+led.direction = digitalio.Direction.OUTPUT 
+print("hello")
+
+
+
+while True: 
+    led.value = True
+    sleep(1)
+    led.value = False
+    sleep(1)
